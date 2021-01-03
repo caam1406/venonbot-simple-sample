958042468c3529ce735a71cd22f28ec905fcd1b5
caam1406 <caam1406@gmail.com>
2021-01-03 14:06:45 -0300

    Finish Sample

---
+++ index.js
@@ -0,0 +1,43 @@
+const venom = require('venom-bot');
+const keywords = require('./keywords');
+
+venom
+	.create()
+	.then((client) => start(client))
+	.catch((erro) => {
+		console.log(erro);
+	});
+
+async function start(client) {
+	await client
+		.sendText('5511982650676@c.us', `👋 The Support System it's online now`)
+
+	client.onMessage((message) => {
+		keywords.welcome.forEach(
+			function (words) {
+				if (message.body === words && message.isGroupMsg === false) {
+					client
+						.sendText(message.from, `Select The Best Option for Your Support:
+1: Receiver a Link Support
+2: Receiver a Video Support
+`)
+				}
+			})
+	});
+	client.onMessage((message) => {
+		if (message.body === '1' && message.isGroupMsg === false) {
+			client
+				.sendText(message.from, `Your Suporte Link Is: https://google.com`)
+		}
+	});
+	client.onMessage((message) => {
+		if (message.body === '2' && message.isGroupMsg === false) {
+			client
+				.sendLinkPreview(
+					message.from,
+					'https://www.youtube.com/watch?v=V1bFr2SWP1I',
+					'Kamakawiwo ole'
+				)
+		}
+	});
+}
---
+++ keywords.js
@@ -0,0 +1,4 @@
+class Keywords {
+    welcome = ['hi','help','whatsapp'];
+}
+module.exports = new Keywords;
